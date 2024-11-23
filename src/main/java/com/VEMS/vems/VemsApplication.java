5aff2cd04a8d5ba60ef576257319ea3d7d442c11
aththas <aththasrizwan123@gmail.com>
2024-11-23 16:42:50 +0530

    initial project with dependencies

---
+++ src/main/java/com/VEMS/vems/VemsApplication.java
@@ -0,0 +1,13 @@
+package com.VEMS.vems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VEMS/vems/VemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VEMS.vems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
